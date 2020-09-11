fe2fe77ea4b3d2cc6d90d018477e9e69167c372f
Deepanjaya <deepanjaya36@gmail.com>
2020-09-11 13:53:47 +0530

    fixed csv

--- mcq.py
+++ mcq.py
@@ -3,22 +3,22 @@ import random
 def quiz():
     score=0
     questionsright=0
-    filename = input(" Please enter the name of the quiz file: ")
+    filename = "Book1.csv"
     quizfile = open(filename, "r")
-    #noofquestions = input(" Please enter the total number of questions to be asked: ")
+    nq = int(input(" Please enter the total number of questions to be asked: "))
     quizdata= quizfile.readlines()
     random.shuffle(quizdata)
     questionno=1
-    for i in range(5):
+    for i in range(nq):
         x= quizdata[i].strip()
         data= x.split(",")
-        question=data[0]
-        Corranswers= data[1]
+        question=data[0]+data[1]+data[2]+data[3]+data[4]
+        Corranswers= data[5]
         
         print("Question #",questionno)
         print(question)
         answer=input("Your answer is(A/B/C/D): ")
-        if(answer == Corranswer):
+        if(answer == Corranswers):
             score=score+1
             questionsright=questionsright+1
             questionno = questionno+1
