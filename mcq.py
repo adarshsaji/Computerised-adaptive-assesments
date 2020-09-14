96b4926bba16ce66b14e3ab1a0f14526bdb9ad46
adarshsaji <sadarsh4@gmail.com>
2020-09-14 12:45:44 +0530

    random + restricted entry

--- mcq.py
+++ mcq.py
@@ -2,13 +2,13 @@ import random
 import xlrd
 
 random_list = []
-
-def random_gen():
-    r = random.random()
+option = ['A','B','C']
+def random_gen(total_ques):
+    r = random.randint(0,total_ques)
     if r not in random_list:
         random_list.append(r)
     else:
-        random_gen()
+        random_gen(total_ques)
     return r
 
 def quiz():
@@ -16,37 +16,39 @@ def quiz():
     questionsright=0
 
     filename = "questions.xlsx"
-    wb = xlrd.open_workbook(filename) 
+    wb = xlrd.open_workbook(filename, encoding_override="utf-8") 
     quizfile = wb.sheet_by_index(0)             #sheet 0 in the excel file
 
+    #to find the total number of questions in the sheet
+    tot_que = len(quizfile.col(0))
+    print("There are a total of {} questions. ".format(tot_que))
     nq = int(input(" Please enter the total number of questions to be asked: "))
     questionno = 1
 
-    #to find the total number of elements in the sheet
-    # total_entries = 0
-    # while (quizfile.cell_value(total_entries,0)) != '':
-    #     total_entries += 1
-    # print(total_entries)
+
 
     for i in range(nq):
-        #r = random_gen()
-        r = nq          #temporary holder for random question number
+        r = random_gen(tot_que-1)
+        #r = i          #temporary holder for random question number
         question = str(quizfile.cell_value(r,0))+"\n A "+ str(quizfile.cell_value(r,1)) + "\n B " + str(quizfile.cell_value(r,2))+ "\n C "+str(quizfile.cell_value(r,3))
         Corranswers = str(quizfile.cell_value(r,5))
-        print(Corranswers)
+        
+        print(Corranswers) #to get ans while testing, comment this before deployment
+
         print("Question #", questionno)
         print(question)
-        answer = input("Your answer is(A/B/C/D): ")
+        answer = input("Your answer is(A/B/C): ")
+        while (answer.upper() not in option):
+             answer = input(answer + " is not a valid option,please choose A, B or C: ") 
         if(answer.upper() == Corranswers):
             score = score+1
-            questionsright = questionsright+1
-            questionno = questionno+1
+            questionsright = questionsright + 1
+            questionno = questionno + 1
         else:
-            print("Incorrect!")
-            questionno = questionno+1
-        print()
-        
-    totalscore = (score/nq)*100
+            questionno = questionno + 1
+            
+    totalscore = (score/nq) * 100
     print("You got ", score, "questions right, and a score of ", totalscore, "%.")
+    print(random_list)
 quiz()
 
