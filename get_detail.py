387c7fa25840b62b553b63eeddbd2a6b046a7231
adarshsaji <sadarsh4@gmail.com>
2020-09-15 23:17:41 +0530

    Added time keeping and saving student response

--- get_detail.py
+++ get_detail.py
@@ -1,14 +1,14 @@
 import openpyxl
 
+filename = 'stud_detail.xlsx'
+wb = openpyxl.load_workbook(filename)
+sheet = wb['Sheet1']
+c_val = (len(sheet['A']))
+
 def get_data():
     name = input("Enter your name: ")
     roll_number = int(input("Enter your roll number: "))
-
-
-    filename = 'stud_detail.xlsx'
-    wb = openpyxl.load_workbook(filename)
-    sheet = wb['Sheet1']
-    c_val = (len(sheet['A']))
+   
     # wb.get_index()
 
     sheet.cell(row=c_val+1 , column=1 ).value = name
@@ -16,3 +16,17 @@ def get_data():
 
     wb.save(filename)
 
+def put_data(responses):
+    studentResponse, timeTaken = responses
+    i = 3       #for data entry from 3rd column
+    j = 0       #for time taken entry
+    for key,value in studentResponse.items():
+         sheet.cell(row = c_val+1 , column = i ).value = key
+         i += 1 
+         sheet.cell(row = c_val+1 , column = i ).value = value
+         i += 1
+         sheet.cell(row = c_val+1 , column = i ).value = timeTaken[j]
+         i += 1
+         j += 1
+         
+    wb.save(filename)
--- mcq.py
+++ mcq.py
@@ -1,23 +1,41 @@
 import random
 import xlrd
+import time
+
 import get_detail
 
-random_list = []
-option = ['A','B','C']
+option = ['A','B','C']          #to validate option input
+time_taken = []                 #to store time taken per question
+# random_list = []
+# def random_gen(total_ques):
+#     r = random.randint(0,total_ques)
+#     if r not in random_list:
+#         random_list.append(r)
+#     else:
+#         random_gen(total_ques)
+#     return r
 
-#random question picker
-def random_gen(total_ques):
-    r = random.randint(0,total_ques)
-    if r not in random_list:
-        random_list.append(r)
-    else:
-        random_gen(total_ques)
-    return r
-    
+def levelup(index):
+    if index <= 2:
+        index += 1
+    elif index == 3:
+        index = 3
+    return index
+
+def leveldown(index):
+    if index >= 2:
+        index -= 1
+    elif index == 1:
+        index = 1
+    return index
+
 def quiz():
-    score=0         #to keep the score
-    questionsright=0
-    get_detail.get_data()
+    score = 0         #to keep the score
+    questionsright = 0
+    que_dict = {}    # Keeps in track the questions answered are correct or incorrect
+
+    get_detail.get_data()  #gets name and roll number from the candidate
+
     filename = "questions.xlsx"
     wb = xlrd.open_workbook(filename, encoding_override="utf-8") 
     quizfile = wb.sheet_by_index(0)             #sheet 0 in the excel file
@@ -28,30 +46,66 @@ def quiz():
     nq = int(input(" Please enter the total number of questions to be asked: "))
     questionno = 1
 
-
-
-    for i in range(nq):
-        r = random_gen(tot_que-1)
-        #r = i          #temporary holder for random question number
-        question = str(quizfile.cell_value(r,0))+"\n A "+ str(quizfile.cell_value(r,1)) + "\n B " + str(quizfile.cell_value(r,2))+ "\n C "+str(quizfile.cell_value(r,3))
-        Corranswers = str(quizfile.cell_value(r,5))
+    Current_level = 2   # initially l
+    index = 0           # (initailly a) used to store the index od question in excel sheet
+    i = 1               # (initailly i) used to iterate nq times 
+    
+    
+    while (i <= nq): # Condition to show only the desired number of questions
         
-        print(Corranswers) #to get ans while testing, comment this before deployment
+        while(index < tot_que) : # Condition to traverse through the available number of questions
+            if(index not in que_dict.keys()): # Check if a certain question has already been asked
+                question = str(quizfile.cell_value(index,0))+"\n A "+ str(quizfile.cell_value(index,1)) + "\n B " + str(quizfile.cell_value(index,2))+ "\n C "+str(quizfile.cell_value(index,3))
+                Corranswers = str(quizfile.cell_value(index,5))
+                level= int(quizfile.cell_value(index,6))
+                timeStart = time.time()
+                
+                if(level != Current_level): # Determines the correct difficulty level question is asked
+                    index += 1
+                
+                else:
+                    t0 = time.time()    
+                    print(Corranswers) #to get ans while testing, comment this before deployment
+                    print("Level",level)
+                    print("Question #", questionno)
+                    print(question)
+                    answer = input("Your answer is(A/B/C): ")
+                    
+                    while (answer.upper() not in option):
+                         answer = input(answer + " is not a valid option,please choose A, B or C: ") 
+                    
+                    if(answer.upper() == Corranswers): # Checks if answer is correct
+                        que_dict[index] = 1
+                        score = score + 1
+                        questionsright = questionsright + 1
+                        questionno = questionno + 1
+                        Current_level = levelup(Current_level)
+                        index += 1
+                        t1 = time.time() - t0
+                        break
+                
+                    else: # Checks if answer is incorrect
+                        que_dict[index] = 0
+                        questionno = questionno + 1
+                        Current_level = leveldown(Current_level)
+                        index += 1
+                        t1 = time.time() - t0 
+                        break
 
-        print("Question #", questionno)
-        print(question)
-        answer = input("Your answer is(A/B/C): ")
-        while (answer.upper() not in option):
-             answer = input(answer + " is not a valid option,please choose A, B or C: ") 
-        if(answer.upper() == Corranswers):
-            score = score+1
-            questionsright = questionsright + 1
-            questionno = questionno + 1
-        else:
-            questionno = questionno + 1
-            
+        time_taken.append(t1)
+        print("Time taken to answer question {} is {:.2f}.".format(questionno-1 , t1))
+        i += 1 
+
+    # to calculate total time taken
+    timeEnd = 0
+    for ele in range(0, len(time_taken)): 
+        timeEnd += time_taken[ele]
+
     totalscore = (score/nq) * 100
     print("You got ", score, "questions right, and a score of ", totalscore, "%.")
-    print(random_list)
-quiz()
+    print("Time taken to finish the test :{:.2f}.".format(timeEnd))
+    print(que_dict)
+    response = que_dict, time_taken
+    get_detail.put_data(response)
 
+quiz()
