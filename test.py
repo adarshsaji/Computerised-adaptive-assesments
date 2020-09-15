87156c86de146dc28a0a0ce64c616e6c228ff4bf
adarshsaji <43480668+adarshsaji@users.noreply.github.com>
2020-09-15 20:39:59 +0530

    refactored

--- test.py
+++ test.py
@@ -2,35 +2,35 @@ import random
 import xlrd
 import time
 
-
 option = ['A','B','C']
-"""random_list = []
-def random_gen(total_ques):
-    r = random.randint(0,total_ques)
-    if r not in random_list:
-        random_list.append(r)
-    else:
-        random_gen(total_ques)
-    return r"""
 
-def levelup(a):
-    if a<=2:
-        a+=1
-    elif a==3:
-        a=3
-    return a
+# random_list = []
+# def random_gen(total_ques):
+#     r = random.randint(0,total_ques)
+#     if r not in random_list:
+#         random_list.append(r)
+#     else:
+#         random_gen(total_ques)
+#     return r
 
-def leveldown(a):
-    if a>=2:
-        a-=1
-    elif a==1:
-        a=1
-    return a
+def levelup(index):
+    if index <= 2:
+        index += 1
+    elif index == 3:
+        index = 3
+    return index
+
+def leveldown(index):
+    if index >= 2:
+        index -= 1
+    elif index == 1:
+        index = 1
+    return index
 
 def quiz():
-    score=0         #to keep the score
-    questionsright=0
-    que_dict={} # Keeps in track the questions answered are correct or incorrect
+    score = 0         #to keep the score
+    questionsright = 0
+    que_dict = {}    # Keeps in track the questions answered are correct or incorrect
 
     filename = "questions.xlsx"
     wb = xlrd.open_workbook(filename, encoding_override="utf-8") 
@@ -41,20 +41,22 @@ def quiz():
     print("There are a total of {} questions. ".format(tot_que))
     nq = int(input(" Please enter the total number of questions to be asked: "))
     questionno = 1
-    l=2
-    a=0
-    i=1
-    while (i<=nq): # Condition to show only the desired number of questions
-        while(a<tot_que) : # Condition to traverse through the available number of questions
-            if(a not in que_dict.keys()): # Check if a certain question has already been asked
-                question = str(quizfile.cell_value(a,0))+"\n A "+ str(quizfile.cell_value(a,1)) + "\n B " + str(quizfile.cell_value(a,2))+ "\n C "+str(quizfile.cell_value(a,3))
-                Corranswers = str(quizfile.cell_value(a,5))
-                level= int(quizfile.cell_value(a,6))
 
-                if(level != l): # Determines the correct difficulty level question is asked
-                    a+=1
+    Current_level = 2   # initially l
+    index = 0           # (initailly a) used to store the index od question in excel sheet
+    i = 1               # (initailly i) used to iterate nq times 
+
+    while (i <= nq): # Condition to show only the desired number of questions
+        while(index < tot_que) : # Condition to traverse through the available number of questions
+            if(index not in que_dict.keys()): # Check if a certain question has already been asked
+                question = str(quizfile.cell_value(index,0))+"\n A "+ str(quizfile.cell_value(index,1)) + "\n B " + str(quizfile.cell_value(index,2))+ "\n C "+str(quizfile.cell_value(index,3))
+                Corranswers = str(quizfile.cell_value(index,5))
+                level= int(quizfile.cell_value(index,6))
+
+                if(level != Current_level): # Determines the correct difficulty level question is asked
+                    index += 1
                 else:
-                    t0= time.time()
+                    t0 = time.time()    
                     print(Corranswers) #to get ans while testing, comment this before deployment
                     print("Level",level)
                     print("Question #", questionno)
@@ -63,26 +65,25 @@ def quiz():
                     while (answer.upper() not in option):
                          answer = input(answer + " is not a valid option,please choose A, B or C: ") 
                     if(answer.upper() == Corranswers): # Checks if answer is correct
-                        que_dict[a]= 1
-                        score = score+1
+                        que_dict[index] = 1
+                        score = score + 1
                         questionsright = questionsright + 1
                         questionno = questionno + 1
-                        l=levelup(l)
-                        a+=1
-                        t1= time.time()-t0
+                        Current_level = levelup(Current_level)
+                        index += 1
+                        t1 = time.time() - t0
                         break
                 
                     else: # Checks if answer is incorrect
-                        que_dict[a]= 0
+                        que_dict[index] = 0
                         questionno = questionno + 1
-                        l=leveldown(l)
-                        a+=1
-                        t1= time.time()-t0
+                        Current_level = leveldown(Current_level)
+                        index += 1
+                        t1 = time.time() - t0 
                         break
         print("Time taken to answer question {} is {}.".format(questionno-1 , t1))
-        i=i+1               
+        i += 1               
     totalscore = (score/nq) * 100
     print("You got ", score, "questions right, and a score of ", totalscore, "%.")
     print(que_dict)
-
 quiz()
