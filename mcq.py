18954054420e01cba9499ccd067101fce6e548f9
adarshsaji <sadarsh4@gmail.com>
2020-09-18 19:56:42 +0530

    added timeout feature

--- mcq.py
+++ mcq.py
@@ -6,6 +6,7 @@ import get_detail
 
 option = ['A','B','C']          #to validate option input
 time_taken = []                 #to store time taken per question
+
 # random_list = []
 # def random_gen(total_ques):
 #     r = random.randint(0,total_ques)
@@ -44,65 +45,73 @@ def quiz():
     tot_que = len(quizfile.col(0))
     print("There are a total of {} questions. ".format(tot_que))
     nq = int(input(" Please enter the total number of questions to be asked: "))
+    test_length = int(input('Enter time constarins of test in minutes: '))
     questionno = 1
 
     Current_level = 2   # initially l
-    index = 0           # (initailly a) used to store the index od question in excel sheet
-    i = 1               # (initailly i) used to iterate nq times 
+    index = 0           # used to store the index od question in excel sheet
+    i = 1               # used to iterate nq times 
     
+    timeout = float(time.time() + 60*test_length)
     
-    while (i <= nq): # Condition to show only the desired number of questions
-        
-        while(index < tot_que) : # Condition to traverse through the available number of questions
-            if(index not in que_dict.keys()): # Check if a certain question has already been asked
-                question = str(quizfile.cell_value(index,0))+"\n A "+ str(quizfile.cell_value(index,1)) + "\n B " + str(quizfile.cell_value(index,2))+ "\n C "+str(quizfile.cell_value(index,3))
-                Corranswers = str(quizfile.cell_value(index,5))
-                level= int(quizfile.cell_value(index,6))
-                timeStart = time.time()
-                
-                if(level != Current_level): # Determines the correct difficulty level question is asked
-                    index += 1
-                
-                else:
-                    t0 = time.time()    
-                    print(Corranswers) #to get ans while testing, comment this before deployment
-                    print("Level",level)
-                    print("Question #", questionno)
-                    print(question)
-                    answer = input("Your answer is(A/B/C): ")
+    while (i <= nq) : # Condition to show only the desired number of questions
+
+        #break the loop if time limit exceeds        
+        end_time = float(time.time())
+        if end_time > timeout :
+            print('Times up!!')
+            break
+
+        else :
+            while(index < tot_que) : # Condition to traverse through the available number of questions
+                if(index not in que_dict.keys()): # Check if a certain question has already been asked
+                    question = str(quizfile.cell_value(index,0))+"\n A "+ str(quizfile.cell_value(index,1)) + "\n B " + str(quizfile.cell_value(index,2))+ "\n C "+str(quizfile.cell_value(index,3))
+                    Corranswers = str(quizfile.cell_value(index,5))
+                    level= int(quizfile.cell_value(index,6))
+                    timeStart = time.time()
                     
-                    while (answer.upper() not in option):
-                         answer = input(answer + " is not a valid option,please choose A, B or C: ") 
+                    if(level != Current_level): # Determines the correct difficulty level question is asked
+                        index += 1
                     
-                    if(answer.upper() == Corranswers): # Checks if answer is correct
-                        que_dict[index] = 1
-                        score = score + 1
-                        questionsright = questionsright + 1
-                        questionno = questionno + 1
-                        Current_level = levelup(Current_level)
-                        index += 1
-                        t1 = time.time() - t0
-                        break
-                
-                    else: # Checks if answer is incorrect
-                        que_dict[index] = 0
-                        questionno = questionno + 1
-                        Current_level = leveldown(Current_level)
-                        index += 1
-                        t1 = time.time() - t0 
-                        break
+                    else:
+                        t0 = time.time()    
+                        print(Corranswers) #to get ans while testing, comment this before deployment
+                        print("Level",level)
+                        print("Question #", questionno)
+                        print(question)
+                        answer = input("Your answer is(A/B/C): ")
+                        
+                        while (answer.upper() not in option):
+                            answer = input(answer + " is not a valid option,please choose A, B or C: ") 
+                        
+                        if(answer.upper() == Corranswers): # Checks if answer is correct
+                            que_dict[index] = 1
+                            score = score + 1
+                            questionsright = questionsright + 1
+                            questionno = questionno + 1
+                            Current_level = levelup(Current_level)
+                            index += 1
+                            t1 = time.time() - t0
+                            break
+                    
+                        else: # Checks if answer is incorrect
+                            que_dict[index] = 0
+                            questionno = questionno + 1
+                            Current_level = leveldown(Current_level)
+                            index += 1
+                            t1 = time.time() - t0 
+                            break
 
         time_taken.append(t1)
         print("Time taken to answer question {} is {:.2f}.".format(questionno-1 , t1))
         i += 1 
 
-    # to calculate total time taken
     timeEnd = 0
     for ele in range(0, len(time_taken)): 
         timeEnd += time_taken[ele]
 
     totalscore = (score/nq) * 100
-    print("You got ", score, "questions right, and a score of ", totalscore, "%.")
+    print("You got {} questions right, and a score of {:.2f}%.".format(score,totalscore))
     print("Time taken to finish the test :{:.2f}.".format(timeEnd))
     print(que_dict)
     response = que_dict, time_taken
